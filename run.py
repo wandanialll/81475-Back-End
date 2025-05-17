65487e8146eb37f056b1ffee0c12765ec2f64fa5
wandanialll <123443949+wandanialll@users.noreply.github.com>
2025-05-17 17:47:36 +0800

    changed cors settings

--- run.py
+++ run.py
@@ -10,10 +10,20 @@ load_dotenv()
 app = create_app()
 
 # Apply CORS globally to all routes
-CORS(app, resources={r"/api/*": {"origins": "https://fyp.wandanial.com"}},
-     supports_credentials=True,
-     allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "X-Requested-With"],
-     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+CORS(app, resources={
+    r"/api/*": {
+        "origins": "https://fyp.wandanial.com",
+        "supports_credentials": True,
+        "allow_headers": [
+            "Content-Type",
+            "Authorization",
+            "Access-Control-Allow-Origin",
+            "Access-Control-Allow-Credentials",
+            "X-Requested-With"
+        ],
+        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    }
+})
 
 if __name__ == "__main__":
     app.run(debug=False)
