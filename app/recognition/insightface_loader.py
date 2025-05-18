99b0f588eddfef4d675602b440210b4744cc03a2
wandanialll <123443949+wandanialll@users.noreply.github.com>
2025-05-19 00:12:16 +0800

    changed some imports path

--- app/recognition/insightface_loader.py
+++ app/recognition/insightface_loader.py
@@ -1,14 +1,20 @@
 import pickle
 from insightface.app import FaceAnalysis
 import numpy as np
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Load the InsightFace model once
 face_app = FaceAnalysis(name='buffalo_l', providers=['CPUExecutionProvider'])
 face_app.prepare(ctx_id=0)
 
-# Load student embeddings from pickle file
-with open("C:\\Users\\dania\\Desktop\\siBijak\\app\\recognition\\insightface_embeddings.pkl", "rb") as f:
+embeedings_path = os.path.join(current_dir, "insightface_embeddings.pkl")
+
+with open(embeedings_path, "rb") as f:
     student_embeddings = pickle.load(f)
+
+
 
 # Normalization (optional if you trained that way)
 def normalize(vec):
